96ab2aea207871d87d0b8bbe7edc08a3053c6d01
RafxDev <jrafaelmorantess@gmail.com>
2026-06-17 21:48:05 -0500

    Resolviendo Mocks, Spies y Ciclo de Vida

--- lessons/04_mocks/service.test.js
+++ lessons/04_mocks/service.test.js
@@ -18,7 +18,7 @@ describe('Lección 4 - Mocking y Ciclo de Vida', () => {
     // RETO 1: Inicializa `mockMailerService` como un objeto que tiene un método `send`.
     // Dicho método `send` debe ser una función mock de Jest.
     // Pista: mockMailerService = { send: jest.fn() };
-    mockMailerService = { send: null }; 
+    mockMailerService = { send: jest.fn() }; 
   });
 
   test('debería enviar la notificación usando el mailerService', () => {
@@ -32,12 +32,12 @@ describe('Lección 4 - Mocking y Ciclo de Vida', () => {
 
     // RETO 2: Verifica que el método `send` de nuestro mockMailerService fue llamado exactamente 1 vez.
     // Pista: Usa el matcher `toHaveBeenCalledTimes` sobre `mockMailerService.send`.
-    expect(mockMailerService.send).toBeDefined();
+    expect(mockMailerService.send).toHaveBeenCalledTimes(1);
 
     // RETO 3: Verifica que `send` fue llamado con los argumentos correctos:
     // 1. El correo generado: 'user1@example.com'
     // 2. El mensaje: 'Hola Mundo'
     // Pista: Usa `toHaveBeenCalledWith` sobre `mockMailerService.send`.
-    expect(mockMailerService.send).toBeDefined();
+    expect(mockMailerService.send).toHaveBeenCalledWith('user1@example.com', 'Hola Mundo');
   });
 });
