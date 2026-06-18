e7b7fae66f673d68ba0ed1d6c809eb522bfab367
RafxDev <jrafaelmorantess@gmail.com>
2026-06-17 21:33:38 -0500

    revolviendo primer problema matematicas

--- lessons/01_basics/math.test.js
+++ lessons/01_basics/math.test.js
@@ -15,13 +15,13 @@ describe('Lección 1 - Operaciones Matemáticas Básicas', () => {
   test('debería sumar dos números correctamente', () => {
     // RETO 1: Reemplaza la línea de abajo para que verifique que 2 + 3 es igual a 5.
     // Pista: Usa expect(sum(2, 3)).toBe(5);
-    expect(sum(2, 3)).toBeUndefined(); // Cambiar esto
+    expect(sum(2, 3)).toBe(5); // Cambiar esto
   });
 
   test('debería restar dos números correctamente', () => {
     // RETO 2: Esta prueba está fallando porque el resultado esperado está mal.
     // Corrige el matcher `.toBe(...)` con el resultado correcto de restar 5 - 2.
-    expect(subtract(5, 2)).toBe(10);
+    expect(subtract(5, 2)).toBe(3);
   });
 
 });
